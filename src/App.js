37d70b301fcac90a105267855fe7a0c163f2cea1
Latif Hidayatullah <latifdayatofficial@gmail.com>
2021-10-14 19:00:36 +0700

    commit for github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { Component } from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
+import Hasil from './components/Hasil';
+import ListCategories from './components/ListCategories';
+import Menus from './components/Menus';
+import NavbarComponent from './components/NavbarComponent'
+import { API_URL } from './utils/constants';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      menus: [],
+      categoriYangDipilih: 'Makanan'
+    }
+  }
+
+  componentDidMount() {
+    axios.get(API_URL + "products?category.nama="+this.state.categoriYangDipilih)
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      //jika gagal
+      .catch(error => {
+        console.log("eror yaaa", error);
+      })
+  }
+
+  changeCategory = (value) => {
+    this.setState({
+      categoriYangDipilih: value,
+      menus: []
+    })
+    axios.get(API_URL + "products?category.nama="+value)
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      //jika gagal
+      .catch(error => {
+        console.log("eror yaaa", error);
+      })
+  }
+
+  render() {
+    const { menus, categoriYangDipilih } = this.state
+    return (
+      <div className="App">
+        <NavbarComponent />
+        <div className="pt-3 color-cream">
+          <Container fluid>
+            <Row>
+              <ListCategories changeCategory={this.changeCategory} categoriYangDipilih={categoriYangDipilih} />
+              <Col>
+                <h4><strong>Daftar Product</strong></h4>
+                <hr />
+                <Container>
+                  <Row>
+                    {menus && menus.map((menu) => (
+                      <Menus
+                        key={menu.id}
+                        menu={menu}
+                      />
+                    ))}
+                  </Row>
+                </Container>
+              </Col>
+              <Hasil />
+            </Row>
+          </Container>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
